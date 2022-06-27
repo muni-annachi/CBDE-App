e2727e40382e9decb21e56bcc81a6ee5b6883e1f
Muni Annachi <mannachi+firstlz@amazon.com>
2022-06-27 11:11:43 -0400

    update code

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Success Response',
+        body: 'Success Response Latest one',
         statusCode: 200,
     };
 };
