7d590b6dfa66bb8edf6df0427d5e1866dbf602ad
Muni Annachi <mannachi+firstlz@amazon.com>
2022-07-05 12:47:10 -0400

    update

--- app.ts
+++ app.ts
@@ -2,8 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from './pipelines_webinar/pipeline_stack';
+import { AwsSolutionsChecks } from 'cdk-nag'
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
+
+//Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}));
 
 new PipelineStack(app, 'PipelineStack', {
     env: {
